6494e86af12baf8ceeb6ea06be6185f7d581e988
chandani31 <137299319+chandani31@users.noreply.github.com>
2023-07-08 12:50:34 +0530

    Add files via upload

---
+++ module1_print_info.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main()
+{
+  print("My Name is Chandani");
+
+  print("My BirthDate is 31'October,2001");
+
+  print("I am 21 years old");
+
+  print("I live in Junagadh");
+}
